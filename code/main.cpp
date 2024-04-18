45454b07aabd9014a3c2e73c0e05b43e94c1b65d
Carson Spaniel <carsonspaniel@gmail.com>
2024-04-18 11:17:12 -0500

    Carson Spaniel: Forgot to add file
    Brief Summary:
    Forgot the main file for testing.
    
    Time Spent:
    0.25
    
    Additional Notes:
    oops

--- code/main.cpp
+++ code/main.cpp
@@ -8,6 +8,18 @@ int main(){
     cout << "Hello Isha!" << "\n";
     cout << "Hello Kaitlyn!" << "\n";
 
-    send_put("Simulation/start",0)
+    cout << "\n\nStarting Simulation" << "\n";
+    send_put("Simulation/start",0);
+
+    cout << "\n\nNext Input" << "\n";
+    send_get("NextInput");
+
+    cout << "\n\nAdding Eric_R to elevator A" << "\n";
+    send_put("AddPersonToElevator/Eric_R/A",0);
+
+    cout << "\n\nElevator A status" << "\n";
+    send_get("ElevatorStatus/A");
+
+    
     return 0;
 }
